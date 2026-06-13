210eea93135fa468dac4521220bc1c1f50409b4c
Kim <kevin2695@naver.com>
2026-06-13 19:30:54 +0900

    fix: 카카오 JS SDK CDN 경로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({
           <ToastContainer />
         </Providers>
         <Script
-          src="https://t1.kakaocdn.net/kakaojs/2.7.4/kakao.min.js"
+          src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.4/kakao.min.js"
           strategy="lazyOnload"
         />
       </body>
